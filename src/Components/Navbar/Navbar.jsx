6ba53e19c0466f983406e350112d62c242a078ba
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-04-14 22:59:12 -0400

    created components

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className='nav-left flex-div'>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar 
