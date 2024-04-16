df86a9596dc1f584b81153b7453956954bd167f4
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-04-16 18:44:44 -0400

    created feed component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Feed from '../../components/feed/Feed'
 
 const Home = ({sidebar}) => {
   return (
     <>
         <Sidebar sidebar={sidebar} />
+        <div className={`container ${sidebar?"":'large-container'}`}>
+            <Feed />
+        </div>
     </>
   )
 }
