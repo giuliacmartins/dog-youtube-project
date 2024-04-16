b4de43c7f989b63a97063e92baffd78855066c91
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-04-16 19:03:51 -0400

    connected api in feed to display in home

--- src/Components/feed/Feed.jsx
+++ src/Components/feed/Feed.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './Feed.css'
 
 const Feed = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            try {
+                const API_KEY = 'AIzaSyAofAxJwSMwwQYiMwcUfLd2Yw183-rZfKo';
+                const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&q=dog`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setVideos(data.items);
+                } else {
+                    console.error('Failed to fetch the videos');
+                }  
+            } catch (error) {
+                console.error('Error fetching the videos:', error);
+            }
+        };
+        fetchVideos();
+    }, []);
+
+    return (
+        <div className="feed">
+            {videos.map((video) => (
+                <div key={video.id.videoId} className="video">
+                <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
+                    <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+                    <div className="video-info">
+                        <h3>{video.snippet.title}</h3>
+                        <p>Channel: {video.snippet.channelTitle}</p>
+                        <p>{video.snippet.description}</p>
+                    </div>
+                </a>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Feed 
