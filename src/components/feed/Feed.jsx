5668eba3091f491c664be97e34328f5503493fdb
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-04-18 00:09:01 -0400

    clean up

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -24,20 +24,6 @@ const Feed = ({ onSelectVideo }) => {
     }, []);
 
     return (
-        // <div className="feed">
-        //     {videos.map((video) => (
-        //         <div key={video.id.videoId} className="video">
-        //         {/* <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer"> */}
-        //         <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
-        //         <div className="video-info">
-        //             <h3>{video.snippet.title}</h3>
-        //             <p>{video.snippet.channelTitle}</p>
-        //             {/* <p>{video.snippet.description}</p> */}
-        //         </div>
-        //         {/* </a> */}
-        //         </div>
-        //     ))}
-        // </div>
         <div className="feed">
             {videos.map((video) => (
                 <div key={video.id.videoId} className="video" onClick={() => onSelectVideo(video)}>
