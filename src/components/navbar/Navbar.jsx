00fe8fbc411d3afd8ed6914be5e1fd018413da65
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2024-04-22 21:01:02 -0400

    more photos and styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import './Navbar.css';
 import menu_icon from '../../assets/menu.png';
 import youtube_logo from '../../assets/youtube_logo.png';
 import search_icon from '../../assets/search.png';
+import create from '../../assets/create.png';
+import bell from '../../assets/bell.png';
+import profile from '../../assets/profile_colored.png';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = ({ setSidebar, setSearchQuery }) => {
@@ -25,22 +28,26 @@ const Navbar = ({ setSidebar, setSearchQuery }) => {
     };
 
     return (
-        <nav className='flex-div'>
-            <div className='nav-left flex-div'>
-                <img className='menu-icon' onClick={() => setSidebar((prev) => !prev)} src={menu_icon} alt='Menu' />
-                <Link to='/'>
-                    <img className='youtube-logo' src={youtube_logo} alt='YouTube' />
+        <nav className="flex-div">
+            <div className="nav-left flex-div">
+                <img className="menu-icon" onClick={() => setSidebar((prev) => !prev)} src={menu_icon} alt="Menu" />
+                <Link to="/">
+                    <img className="youtube-logo" src={youtube_logo} alt="YouTube" />
                 </Link>
             </div>
-            <div className='nav-middle flex-div'>
-                <div className='search-bar flex-div'>
-                    <input type='text' placeholder='Search' value={searchInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
-                    <img className='search-icon' src={search_icon} alt='Search' onClick={handleSearch} />
+            <div className="nav-middle flex-div">
+                <div className="search-bar flex-div">
+                    <input type="text" placeholder="Search" value={searchInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
+                    <img className="search-icon" src={search_icon} alt="Search" onClick={handleSearch} />
                 </div>
             </div>
-            <div className='nav-right flex-div'>{/* Right side of the navbar */}</div>
+            <div className="nav-right flex-div">
+                <img src={create} alt="Create" />
+                <img src={bell} alt="Notification" />
+                <img className="profile" src={profile} alt="Profile" />
+            </div>
         </nav>
-    );
+    ); 
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Sidebar.css'
 import home_icon from '../../assets/home.png'
+import subscriptions from '../../assets/subscriptions.png'
+import your_channel from '../../assets/your_channel.png'
+import history from '../../assets/history.png'
+import playlist from '../../assets/playlist.png'
+import your_videos from '../../assets/your_videos.png'
+import watch_later from '../../assets/watch_later.png'
+import liked_videos from '../../assets/like_videos.png'
+import profile from '../../assets/profile_colored.png'
 import { Link } from 'react-router-dom';
 
 const Sidebar = ({sidebar}) => {
@@ -8,12 +16,67 @@ const Sidebar = ({sidebar}) => {
         <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
             <div className="shortcut">
                 <Link to='/'>
-                    <div className="side-links">
+                    <div className="side-link">
                         <img src={home_icon} alt="Home" />
                         <p>Home</p>
-                        {/* might add more to the sidebar */}
                     </div>
                 </Link>
+                <div className="side-link">
+                    <img src={subscriptions} alt="Subscriptions" />
+                    <p>Subscriptions</p>
+                </div>
+                <hr/>
+            </div>
+            <div className="you">
+                <h3>You</h3>
+                <div className="side-link">
+                    <img src={your_channel} alt="Your channel" />
+                    <p>Your channel</p>
+                </div>
+                <div className="side-link">
+                    <img src={history} alt="History" />
+                    <p>History</p>
+                </div>
+                <div className="side-link">
+                    <img src={playlist} alt="Playlists" />
+                    <p>Playlists</p>
+                </div>
+                <div className="side-link">
+                    <img src={your_videos} alt="Your videos" />
+                    <p>Your videos</p>
+                </div>
+                <div className="side-link">
+                    <img src={watch_later} alt="Watch Later" />
+                    <p>Watch later</p>
+                </div>
+                <div className="side-link">
+                    <img src={liked_videos} alt="Liked videos" />
+                    <p>Liked videos</p>
+                </div>
+                <hr/>
+            </div>
+            <div className="subscribed">
+                <h3>Subscriptions</h3>
+                <div className="side-link">
+                    <img src={profile} alt="Profile" />
+                    <p>Markiplier</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile} alt="Profile" />
+                    <p>isaacwhy</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile} alt="Profile" />
+                    <p>Smosh Games</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile} alt="Profile" />
+                    <p>Jacksepticeye</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile} alt="Profile" />
+                    <p>Smosh</p>
+                </div>
             </div>
         </div>
     )
